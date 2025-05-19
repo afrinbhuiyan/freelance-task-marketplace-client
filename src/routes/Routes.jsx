0085fdf6a5872914e2d34a873999faf60de5dbb1
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2025-05-20 00:21:45 +0600

    Added new routes and secured credentials.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,57 @@
 import { createBrowserRouter } from "react-router";
 import mainLayout from "../layout/mainLayout";
+import PrivateRoute from "../provider/PrivateRoute";
+import AddTask from "../pages/AddTask";
+import MyTasks from "../pages/MyTasks";
+import TaskDetails from "../pages/TaskDetails";
+import UpdateTask from "../pages/UpdateTask";
+import BrowseTasks from "../pages/BrowseTasks";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: mainLayout
+    Component: mainLayout,
+    children: [
+      { path: "/", Component: Home },
+      { path: "/login", Component: Login },
+      { path: "/signup", Component: Signup },
+      {
+        path: "/add-task",
+        element: (
+          <PrivateRoute>
+            <AddTask />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/my-tasks",
+        element: (
+          <PrivateRoute>
+            <MyTasks />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/task/:id",
+        element: (
+          <PrivateRoute>
+            <TaskDetails />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/update-task/:id",
+        element: (
+          <PrivateRoute>
+            <UpdateTask />
+          </PrivateRoute>
+        ),
+      },
+      { path: "/browse-tasks", Component: BrowseTasks },
+    ],
   },
 ]);
 
